187fe2c3ccf9c156ae3a357feebcdbaf5a038de5
Aman Verma <amanvermaspace@gmail.com>
2024-03-25 12:37:58 +0530

    Completed the functionality of input changing colors

--- footerApp/src/components/Footer.jsx
+++ footerApp/src/components/Footer.jsx
@@ -1,8 +1,41 @@
 import "./Footer.css";
 
+import isEmail from "validator/lib/isEmail";
+
 import React from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Footer = () => {
+  const [emailValue, setEmailValue] = useState("");
+
+  const inpRef = useRef();
+
+  const inputRef = useRef();
+  useEffect(() => {
+    if (emailValue == "") {
+      inputRef.current.style.background = linearGradientUnedited;
+      inputRef.current.style.borderColor = "#2478d7";
+    } else if (isEmail(emailValue)) {
+      inputRef.current.style.background = linearGradientValid;
+      inputRef.current.style.borderColor = "#58ef0d";
+    } else {
+      inputRef.current.style.background = linearGradientInvalid;
+      inputRef.current.style.borderColor = "#c41010";
+    }
+  }, [emailValue]);
+
+  //? LINEAER GRADIENTS
+  const linearGradientUnedited =
+    "linear-gradient(90deg, #2478d7, #2478d7,  #002b5c, #000)";
+  const linearGradientValid =
+    "linear-gradient(90deg, #58ef0d, #58ef0d,  #2A7F00, #000)";
+  const linearGradientInvalid =
+    "linear-gradient(90deg, #c41010, #c41010,  #7A0000, #000)";
+
+  function inputChangeHandler(value) {
+    setEmailValue(value);
+  }
+
   return (
     <div className="footerContainer">
       <nav>
@@ -19,13 +52,26 @@ const Footer = () => {
       <div className="contactContent">
         <div className="contactContentLeft">
           <h1>Contacts</h1>
-          <p>+1000000000</p>
-          <p>Basedd in Netherlands</p>
+          <p>+1&nbsp;00&nbsp;000&nbsp;00&nbsp;00</p>
+          <p>Based in Netherlands</p>
         </div>
         <div className="contactContentRight">
-          <div className="inputContainer">
-            <input type="email" name="" id="" />
-            <button>Copy</button>
+          <div
+            ref={inputRef}
+            className="inputContainer"
+            style={{
+              background: linearGradientUnedited,
+            }}>
+            <input
+              type="email"
+              name=""
+              id="emailField"
+              placeholder="email"
+              ref={inpRef}
+              value={emailValue}
+              onChange={(event) => inputChangeHandler(event.target.value)}
+            />
+            <button>Submit</button>
           </div>
           <div className="socialMedia">
             <a href="https://www.linkedin.com/in/aman-verma-82b823185/">
