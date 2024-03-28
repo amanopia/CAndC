c36343b13f524c757dd7f96123ce1d0077390c23
Aman Verma <amanvermaspace@gmail.com>
2024-03-28 17:43:58 +0530

    completed the header section

--- portfolio/src/Header/Header.jsx
+++ portfolio/src/Header/Header.jsx
@@ -4,6 +4,7 @@ import myImg from "../assets/my_img1.png";
 import social from "../assets/social-media.png";
 import videoC from "../assets/vc.png";
 import chat from "../assets/chat.png";
+import "./Header.css";
 const Header = () => {
   return (
     <div className="">
@@ -12,35 +13,43 @@ const Header = () => {
         src={abs}
         alt="Abstract"
       />
-
-      <div className="m-5 p-4 h-full text-center sm:text-left">
-        {/* CONTENT LEFT */}
-        <div className="flex flex-col items-center top:36 sm:relative sm:h-28">
-          <img
-            className="object-cover w-min rounded-full border-4 border-white h-40 md:h-32 lg:h-64 sm:absolute sm:-top-20 sm:left-0"
-            src={myImg}
-            alt=""
-          />
-          <div className="my-12 relative sm:-top-16 sm:left-8 ">
-            <h1 className="text-2xl md:text-xl font-semibold ">Aman Verma</h1>
-            <p className="text-sm my-2 md:text-xs font-light text-slate-400">
-              I am a Full Stack Developer based in Delhi.
-            </p>
+      {/* OUTER BOX FOR PADDING AND MARGIN*/}
+      <div className="relative items-center m-auto h-80">
+        {/* INNER BOX CONTAINING ALL THE COTENTS SHIFTED TO TOP-16 */}
+        <div
+          className="absolute -top-16 left-0 right-0 m-auto text-center flex flex-col items-center 
+        sm:items-start sm:-top-8 sm:mx-5 md:flex-row md:items-center md:justify-between xl:-top-12">
+          {/* CONTENT LEFT */}
+          <div className="flex flex-col items-center sm:flex-row sm:gap-x-6">
             <img
-              src={social}
-              className="h-10 absolute -top-20 right-20 sm:top-12 sm:-left-16"></img>
+              className="object-cover w-min rounded-full border-4 border-white h-40 md:h-32 lg:h-32 xl:h-56 "
+              src={myImg}
+              alt="pfp"
+            />
+            <div className="relative my-12 sm:text-left">
+              <h1 className="text-2xl font-semibold md:text-xl xl:text-4xl">
+                Aman Verma
+              </h1>
+              <p className="text-sm my-2 font-light text-slate-400 md:text-xs xl:text-base">
+                I am a Full Stack Developer based in Delhi.
+              </p>
+              <img
+                src={social}
+                alt="tick"
+                className="h-10 absolute -top-20 right-20 sm:top-16 sm:-left-24 md:top-12 md:-left-16 xl:h-12"></img>
+            </div>
           </div>
-        </div>
-        {/* CONTENT RIGHT */}
-        <div className="right-10 flex gap-x-6 sm:mx-4">
-          <button className="text-sm my-2 md:text-xs flex items-center gap-x-2 border-2 px-4 py-2 rounded-md bg-white hover:invert transition-all">
-            <img src={videoC} alt="video call" className="h-4" />
-            Video call
-          </button>
-          <button className="text-sm my-2 md:text-xs flex items-center gap-x-2 bg-black px-4 py-2 text-white rounded-md hover:invert border-2 transition-all border-white">
-            <img src={chat} alt="message" className="h-4" />
-            Message
-          </button>
+          {/* CONTENT RIGHT */}
+          <div className="right-10 flex gap-x-6 sm:mx-4">
+            <button className="text-sm my-2 md:text-xs flex items-center gap-x-2 border-2 px-4 py-2 rounded-md bg-white hover:invert transition-all">
+              <img src={videoC} alt="video call" className="h-4 " />
+              Video call
+            </button>
+            <button className="text-sm my-2 md:text-xs flex items-center gap-x-2 bg-black px-4 py-2 text-white rounded-md hover:invert border-2 transition-all border-white">
+              <img src={chat} alt="message" className="h-4" />
+              Message
+            </button>
+          </div>
         </div>
       </div>
     </div>
